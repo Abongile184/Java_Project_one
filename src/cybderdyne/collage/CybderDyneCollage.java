5e0355cc16228c808a8fbe278fee07d45d7dded8
Abongile184 <74903148+Abongile184@users.noreply.github.com>
2023-09-24 14:28:34 +0200

    Add files via upload

---
+++ src/cybderdyne/collage/CybderDyneCollage.java
@@ -0,0 +1,30 @@
+/*Visual Basic Assignment 
+
+Cyberdyne Collage is a registered private institute of learning specializing in commerce and computing.
+Ever since its inception in 2000, the collage administration has been using a manual system of records
+for student enquiries, enrollment (using the old fashioned point system) and registration of new students.
+All of the above mentioned procedures make use of the level arch filling system
+This system has since become obsolete inconsistance and unreliable.
+For Example: If the collage administration needs to go back to the students records he will need to go
+and check on all the dusty files in the collage storeroom, sometimes this effort will be to no avail 
+as some records cannot be found.
+ 
+ */
+package cybderdyne.collage;
+
+/**
+ *
+ * @author Abongile Mavela
+ */
+public class CybderDyneCollage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     frmMainLogin frmMainLogin = new frmMainLogin();
+     frmMainLogin.show();
+     
+    }
+    
+}
